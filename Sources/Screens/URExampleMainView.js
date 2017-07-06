e901a4137f7b41402df5ac1344d1571297a6b615
DongSoo Lee <bryanlee@zigbang.com>
2017-07-06 14:18:55 +0900

    test react native blurview

--- Sources/Screens/URExampleMainView.js
+++ Sources/Screens/URExampleMainView.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { 
   StyleSheet, 
-  Text, View, 
+  View, 
+  Text, 
   Button, 
   ListView, 
   Image,
   Animated
 } from 'react-native';
+import { BlurView } from 'react-native-blur'
 
 import URExampleSampleCell from '../URExampleCell';
 import ImageAssets from './../../ImageAssets'
@@ -40,7 +42,7 @@ export class URExampleMainView extends React.Component {
     const { navigate } = this.props.navigation
     const opacity = {opacity: this.state.opacity}
     return (
-      <View style={styles.container}>
+      <Animated.View style={[styles.container, opacity]}>
         <Button
           onPress={this.onPressLearnMore.bind(this)}
           title="Learn More"
@@ -48,25 +50,24 @@ export class URExampleMainView extends React.Component {
           accessibilityLabel="Learn more about this purple button"
         />
         { this.state.movableView }
-        <Animated.View style={[styles.subContainer, opacity]}>
-        <ListView ref={(list) => this.list = list} style={styles.list}
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => {
-              console.log('rowData is ' + rowData.title + ', ' + rowData.img)
-            return (
-              <URExampleSampleCell data={rowData} title={rowData.title} 
-              img={rowData.img} 
-              onPress={this.onPressRow.bind(this)} 
+          <View style={styles.subContainer}>
+            <ListView ref={(list) => this.list = list} style={styles.list}
+              dataSource={this.state.dataSource}
+              renderRow={(rowData) => {
+                console.log('rowData is ' + rowData.title + ', ' + rowData.img)
+                return (
+                  <URExampleSampleCell data={rowData} title={rowData.title} 
+                  img={rowData.img} 
+                  onPress={this.onPressRow.bind(this)} 
+                  />
+                )
+                }
+              }
+              onScroll={this._onScroll}
+              onScrollAnimationEnd={this._onScrollAnimationEnd}
               />
-              )
-            }
-          }
-          onScroll={this._onScroll}
-          onScrollAnimationEnd={this._onScrollAnimationEnd}
-
-        />
-        </Animated.View>
-      </View>
+        </View>
+      </Animated.View>
     )
   }
 
@@ -117,9 +118,10 @@ export class URExampleMainView extends React.Component {
     const movableView = this.state.movableView
     if (Array.isArray(movableView)) {
       movableView.push(
-        <View key={movableView.length} style={styles.movableView}>
+        <BlurView key={movableView.length} style={styles.movable}
+        blurType="light">
           <Text>얍얍</Text>
-        </View>
+        </BlurView>
       )
     }
 
@@ -156,6 +158,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: '100%',
     height: '100%',
-    backgroundColor: '#0aa'
+    backgroundColor: '#fff'
   }
 });
